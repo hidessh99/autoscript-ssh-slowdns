5975fcdc9daa7dc664659d6fe1e829287cbc6267
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-09-28 06:52:21 +0700

    Create slowdns.sh

---
+++ slowdns.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+# Script  By nstaller slowdns
+# 2022 SLOWDNS
+# ===============================================
+
+
+#tambah dns
+#sudo systemctl disable systemd-resolved.service && sudo systemctl stop systemd-resolved.service && sudo mv /etc/resolv.conf /etc/resolv.conf.bkp && echo "nameserver 1.1.1.1" > /etc/resolv.conf
+#sudo systemctl enable systemd-resolved.service && sudo systemctl start systemd-resolved.service
+#sleep 2
+
+#setting IPtables
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+
+cd
+#delete directory
+rm -rf /root/nsdomain
+rm nsdomain
+
+#input nameserver manual to cloudflare
+read -rp "Masukkan domain: " -e domain
+
+read -rp "Masukkan Subdomain: " -e sub
+SUB_DOMAIN=${sub}.${domain}
+NS_DOMAIN=slowdns-${SUB_DOMAIN}
+echo $NS_DOMAIN > /root/nsdomain
+
+nameserver=$(cat /root/nsdomain)
+apt update -y
+apt install -y python3 python3-dnslib net-tools
+apt install ncurses-utils -y
+apt install dnsutils -y
+#apt install golang -y
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install ncurses-utils -y
+apt install screen -y
+apt install cron -y
+apt install iptables -y
+apt install -y git screen whois dropbear wget
+#apt install -y pwgen python php jq curl
+apt install -y sudo gnutls-bin
+#apt install -y mlocate dh-make libaudit-dev build-essential
+apt install -y dos2unix debconf-utils
+service cron reload
+service cron restart
+
+#tambahan port openssh
+cd
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 2269" >> /etc/ssh/sshd_config
+sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+service ssh restart
+service sshd restart
+
+#konfigurasi slowdns
+rm -rf /etc/slowdns
+mkdir -m 777 /etc/slowdns
+wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.key"
+wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.pub"
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-server"
+wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-client"
+cd
+chmod +x /etc/slowdns/server.key
+chmod +x /etc/slowdns/server.pub
+chmod +x /etc/slowdns/sldns-server
+chmod +x /etc/slowdns/sldns-client
+
+cd
+#wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/client-sldns.service"
+#wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server-sldns.service"
+
+cd
+#install client-sldns.service
+cat > /etc/systemd/system/client-sldns.service << END
+[Unit]
+Description=Client SlowDNS By HideSSH
+Documentation=https://hidessh.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:2222
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+cd
+#install server-sldns.service
+cat > /etc/systemd/system/server-sldns.service << END
+[Unit]
+Description=Server SlowDNS By HideSSH
+Documentation=https://hidessh.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+#permission service slowdns
+cd
+chmod +x /etc/systemd/system/client-sldns.service
+
+chmod +x /etc/systemd/system/server-sldns.service
+pkill sldns-server
+pkill sldns-client
+
+systemctl daemon-reload
+systemctl stop client-sldns
+systemctl stop server-sldns
+
+systemctl enable client-sldns
+systemctl enable server-sldns
+
+systemctl start client-sldns
+systemctl start server-sldns
+
+systemctl restart client-sldns
+systemctl restart server-sldns
